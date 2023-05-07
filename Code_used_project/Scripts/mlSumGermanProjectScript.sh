206fc6eb72248a093e9c5ec67f08949d2b49f7cd
Shiva Shukla <shiva27295@gmail.com>
2023-05-06 20:47:49 -0400

    updated results and german script file

--- Code_used_project/Scripts/mlSumGermanProjectScript.sh
+++ Code_used_project/Scripts/mlSumGermanProjectScript.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks-per-node=12
 
 #SBATCH --mem=150GB
-#SBATCH --time=0-12:00:00
+#SBATCH --time=0-14:00:00
 
 #SBATCH --output=mlSum88866_German.%j.out
 #SBATCH --error=mlSum88866_German.%j.err
@@ -18,4 +18,4 @@ module load python
 
 pip install --upgrade pip
 pip install -r requirementsmlSum.txt
-python run_summarization_no_trainer_mlSum_German.py --model_name_or_path Shahm/bart-german --dataset_name mlsum --dataset_config_name de --pred_distill --intermediate_distill --num_train_epochs 5 --weight_bits 8 --do_train --do_test --distill_encoder 6 --distill_decoder 6 --learning_rate 5e-5 --per_device_train_batch_size 6 --seed 42
+python run_summarization_no_trainer_mlSum_German.py --model_name_or_path Shahm/bart-german --dataset_name mlsum --dataset_config_name de --pred_distill --intermediate_distill --num_train_epochs 5 --weight_bits 8 --do_train --do_test --distill_encoder 3 --distill_decoder 1 --learning_rate 5e-5 --per_device_train_batch_size 6 --seed 42
